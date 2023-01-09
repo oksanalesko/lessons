5b5f1ad5ce9f9e7a36e83b60730b37a04c51a999
oksanalesko <108001051+oksanalesko@users.noreply.github.com>
2023-01-09 16:40:17 +0200

    Add files via upload

---
+++ lesson_22hw/js/script.js
@@ -0,0 +1,39 @@
+"use strict"
+
+console.log('Я вчу JS');
+
+let colorUsersEyes;
+
+let user;
+let userName;
+user = 'Вася';
+userName = user;
+console.log(userName);
+
+// let user = 'Вася' let age = 36
+
+// let myage = 36;
+
+// const BLOCK_SIZE = 14 +50;
+
+const BLOCK_HEIGHT = 100;
+
+if (true) {
+   var size = 15;
+}
+console.log(size);
+
+// let userAge = 36;
+// let userInfo = "Фрілансер ${userAge}";
+// console.log(userInfo);
+
+// let userHeight = 145 / 0;
+// console.log(userHeight);
+
+// let userName;
+// console.log(typeof userName);
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
+
+// ==================================================
